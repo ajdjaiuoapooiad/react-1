76fdf3a2e0fabdb3084a893d689e65f33f6c99ef
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-12 11:52:27 +0900

    todo ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
+import { useState } from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [ todo,setTodo ] = useState(['Todo1','Todo2']);
+
   return (
     <div>
-      <TodoList />
+      <TodoList todo={todo} />
+      <input type='text' />
+      <button>タスクを追加</button>
+      <button>タスクの削除</button>
+      <div>残りのタスク: 0</div>
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Todo from './Todo';
 
-const TodoList = () => {
-  return (
-    <div>TodoList</div>
-  )
-}
+
+
+const TodoList = ({todo}) => {
+  return todo.map((todo) => <Todo todo={todo} />)
+};
 
 export default TodoList
